f18a5bf8ef2c30ba1279c17db5dd1edd0f4979d7
kg9903 <36208392+kg9903@users.noreply.github.com>
2023-04-01 02:11:53 -0400

    add writing and libs

--- temp/Debug/main.d
+++ temp/Debug/main.d
@@ -1,36 +1,183 @@
 # FIXED
 
 main.obj: ../main.c
-main.obj: C:/ti/ccs1210/ccs/ccs_base/msp430/include/msp430.h
-main.obj: C:/ti/ccs1210/ccs/ccs_base/msp430/include/msp430fr5994.h
-main.obj: C:/ti/ccs1210/ccs/ccs_base/msp430/include/in430.h
-main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h
-main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h
-main.obj: C:/ti/ccs1210/ccs/ccs_base/msp430/include/legacy.h
-main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdlib.h
-main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_ti_config.h
-main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/linkage.h
-main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/cdefs.h
+main.obj: /Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdint.h
+main.obj: /Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_ti_config.h
+main.obj: /Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/linkage.h
+main.obj: /Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_stdint40.h
+main.obj: /Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/stdint.h
+main.obj: /Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/cdefs.h
+main.obj: /Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/_types.h
+main.obj: /Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_types.h
+main.obj: /Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_stdint.h
+main.obj: /Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/_stdint.h
+main.obj: /Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/string.h
+main.obj: /Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/xlocale/_string.h
+main.obj: /Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdlib.h
+main.obj: /Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdio.h
+main.obj: /Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdarg.h
+main.obj: ../driverlib/driverlib.h
+main.obj: ../driverlib/inc/hw_memmap.h
+main.obj: /Applications/ti/ccs1210/ccs/ccs_base/msp430/include/msp430.h
+main.obj: /Applications/ti/ccs1210/ccs/ccs_base/msp430/include/msp430fr5994.h
+main.obj: /Applications/ti/ccs1210/ccs/ccs_base/msp430/include/in430.h
+main.obj: /Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h
+main.obj: /Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h
+main.obj: /Applications/ti/ccs1210/ccs/ccs_base/msp430/include/legacy.h
+main.obj: /Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdbool.h
+main.obj: ../driverlib/esi.h
+main.obj: ../driverlib/eusci_b_i2c.h
+main.obj: ../driverlib/cs.h
+main.obj: ../driverlib/aes256.h
+main.obj: ../driverlib/uups.h
+main.obj: ../driverlib/framctl_a.h
+main.obj: ../driverlib/timer_b.h
+main.obj: ../driverlib/framctl.h
+main.obj: ../driverlib/eusci_a_spi.h
+main.obj: ../driverlib/eusci_a_uart.h
+main.obj: ../driverlib/tlv.h
+main.obj: ../driverlib/timer_a.h
+main.obj: ../driverlib/pmm.h
+main.obj: ../driverlib/adc12_b.h
+main.obj: ../driverlib/sysctl.h
+main.obj: ../driverlib/sfr.h
+main.obj: ../driverlib/ref_a.h
+main.obj: ../driverlib/mpy32.h
+main.obj: ../driverlib/wdt_a.h
+main.obj: ../driverlib/saph.h
+main.obj: ../driverlib/crc32.h
+main.obj: ../driverlib/mpu.h
+main.obj: ../driverlib/mtif.h
+main.obj: ../driverlib/lcd_c.h
+main.obj: ../driverlib/rtc_c.h
+main.obj: ../driverlib/gpio.h
+main.obj: ../driverlib/rtc_b.h
+main.obj: ../driverlib/comp_e.h
+main.obj: ../driverlib/ram.h
+main.obj: ../driverlib/saph_a.h
+main.obj: ../driverlib/dma.h
+main.obj: ../driverlib/hspll.h
+main.obj: ../driverlib/eusci_b_spi.h
+main.obj: ../driverlib/sdhs.h
+main.obj: ../driverlib/crc.h
 
 ../main.c:
 
-C:/ti/ccs1210/ccs/ccs_base/msp430/include/msp430.h:
+/Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdint.h:
 
-C:/ti/ccs1210/ccs/ccs_base/msp430/include/msp430fr5994.h:
+/Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_ti_config.h:
 
-C:/ti/ccs1210/ccs/ccs_base/msp430/include/in430.h:
+/Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/linkage.h:
 
-C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h:
+/Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_stdint40.h:
 
-C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h:
+/Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/stdint.h:
 
-C:/ti/ccs1210/ccs/ccs_base/msp430/include/legacy.h:
+/Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/cdefs.h:
 
-C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdlib.h:
+/Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/_types.h:
 
-C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_ti_config.h:
+/Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_types.h:
 
-C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/linkage.h:
+/Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_stdint.h:
 
-C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/cdefs.h:
+/Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/_stdint.h:
+
+/Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/string.h:
+
+/Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/xlocale/_string.h:
+
+/Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdlib.h:
+
+/Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdio.h:
+
+/Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdarg.h:
+
+../driverlib/driverlib.h:
+
+../driverlib/inc/hw_memmap.h:
+
+/Applications/ti/ccs1210/ccs/ccs_base/msp430/include/msp430.h:
+
+/Applications/ti/ccs1210/ccs/ccs_base/msp430/include/msp430fr5994.h:
+
+/Applications/ti/ccs1210/ccs/ccs_base/msp430/include/in430.h:
+
+/Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h:
+
+/Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h:
+
+/Applications/ti/ccs1210/ccs/ccs_base/msp430/include/legacy.h:
+
+/Applications/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdbool.h:
+
+../driverlib/esi.h:
+
+../driverlib/eusci_b_i2c.h:
+
+../driverlib/cs.h:
+
+../driverlib/aes256.h:
+
+../driverlib/uups.h:
+
+../driverlib/framctl_a.h:
+
+../driverlib/timer_b.h:
+
+../driverlib/framctl.h:
+
+../driverlib/eusci_a_spi.h:
+
+../driverlib/eusci_a_uart.h:
+
+../driverlib/tlv.h:
+
+../driverlib/timer_a.h:
+
+../driverlib/pmm.h:
+
+../driverlib/adc12_b.h:
+
+../driverlib/sysctl.h:
+
+../driverlib/sfr.h:
+
+../driverlib/ref_a.h:
+
+../driverlib/mpy32.h:
+
+../driverlib/wdt_a.h:
+
+../driverlib/saph.h:
+
+../driverlib/crc32.h:
+
+../driverlib/mpu.h:
+
+../driverlib/mtif.h:
+
+../driverlib/lcd_c.h:
+
+../driverlib/rtc_c.h:
+
+../driverlib/gpio.h:
+
+../driverlib/rtc_b.h:
+
+../driverlib/comp_e.h:
+
+../driverlib/ram.h:
+
+../driverlib/saph_a.h:
+
+../driverlib/dma.h:
+
+../driverlib/hspll.h:
+
+../driverlib/eusci_b_spi.h:
+
+../driverlib/sdhs.h:
+
+../driverlib/crc.h:
 
--- temp/main.c
+++ temp/main.c
@@ -2,9 +2,15 @@
 //  ACLK = n/a, MCLK = SMCLK = default DCO
 //
 //***************************************************************************************
-
-#include <msp430.h>
+#include <stdint.h>
+#include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "driverlib/driverlib.h"
+#include <msp430.h>
+
+#define CLK_SPEED_8MHz      1
+#define CLK_SPEED_16MHz     0
 
 unsigned int adc_raw = 0;
 unsigned int prev_raw = 0;
@@ -60,6 +66,53 @@ void matMul(void){
 }
 
 void clock_1(){
+    // Configure GPIO
+        P1OUT = 0;
+        P1DIR = 0xFF;
+
+        P1OUT = 0;
+        P1DIR = 0xFF;
+
+        P2OUT = 0;
+        P2DIR = 0xFF;
+
+        P4OUT = 0;
+        P4DIR = 0xFF;
+
+        P5OUT = 0;
+        P5DIR = 0xFF;
+
+        P6OUT = 0;
+        P6DIR = 0xFF;
+
+        P7OUT = 0;
+        P7DIR = 0xFF;
+
+        P8DIR = 0xFF;
+        P8OUT = 0;
+
+        P9DIR = 0xFF;
+        P9OUT = 0;
+
+        PADIR = 0xFF;
+        PAOUT = 0;
+
+        PBDIR = 0xFF;
+        PBOUT = 0;
+
+        PCDIR = 0xFF;
+        PCOUT = 0;
+
+        PDDIR = 0xFF;
+        PDOUT = 0;
+
+        PEDIR = 0xFF;
+        PEOUT = 0;
+
+        PJOUT = 0;
+        PJSEL0 = BIT4 | BIT5;                   // For XT1
+        PJDIR = 0xFFFF;
+
     P3DIR |= BIT4;
     P3SEL1 |= BIT4;                         // Output SMCLK
     P3SEL0 |= BIT4;
@@ -70,45 +123,55 @@ void clock_1(){
     // Set SMCLK = MCLK = DCO, ACLK = VLOCLK
     CSCTL2 = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;
 
+    FRCTL0 = FRCTLPW | NWAITS_6; // above 16Mhz needs FRAM wait states
+
     // Per Device Errata set divider to 4 before changing frequency to
     // prevent out of spec operation from overshoot transient
     CSCTL3 = DIVA__4 | DIVS__4 | DIVM__4;   // Set all corresponding clk sources to divide by 4 for errata
     CSCTL1 = DCOFSEL_6;                     // Set DCO to 8MHz ? 24 MHz
-    //CSCTL1 ^= DCORSEL;                     // Set DCO to 24 MHz
+    CSCTL1 |= DCORSEL;                     // Set DCO to 24 MHz
 
     // Delay by ~10us to let DCO settle. 60 cycles = 20 cycles buffer + (10us / (1/4MHz))
     __delay_cycles(180);
-    CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;   // Set all dividers to 1 for 8MHz operation
+    //CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;   // Set all dividers to 1 for 8MHz operation
     CSCTL0_H = 0;                           // Lock CS Registers
 }
 
-int timerCounter = 0;
+void clock_setup(void){
 
-void main(void) {
-    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
-    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
-                                            // to activate previously configured port settings
-    P3DIR |= 0x01;                          // Set P1.0 to output direction
-    //P1DIR |= 0x01;                          // Set P1.0 to output direction
+#if CLK_SPEED_8MHz
+    //Set DCO Frequency to 8MHz
+    CS_setDCOFreq(CS_DCORSEL_0, CS_DCOFSEL_6);
 
-    TA0CCTL0 = CCIE;                        // TACCR0 interrupt enabled
-    TA0CCR0 = 50000;
-    TA0CTL = TASSEL__SMCLK | MC__CONTINOUS; // SMCLK, continuous mode
+    //configure MCLK, SMCLK to be source by DCOCLK
+    CS_initClockSignal(CS_MCLK,  CS_DCOCLK_SELECT,  CS_CLOCK_DIVIDER_1); //16mhz
+    CS_initClockSignal(CS_SMCLK, CS_DCOCLK_SELECT,  CS_CLOCK_DIVIDER_1); // 16mhz
 
-    __bis_SR_register(LPM0_bits | GIE);     // Enter LPM0 w/ interrupt
+#endif
 
-    clock_1();
-    /*
-    UCA0CTLW0 = UCSWRST;                    //reset states
-    UCA0CTLW0 |= UCSSEL_2;                  //eUSCI clock source: SMCLK
-    UCA0BRW = 6;
-    UCA0MCTLW = UCOS16 | (8 << 4) | (32 << 8); //set baud rate to 9600 8N1
-    P2SEL1 |= BIT0 | BIT1;
-    P2SEL0 &= ~(BIT0 | BIT1);
-    UCA0CTLW0 &= ~UCSWRST;*/
-    __delay_cycles(2000);
+#if CLK_SPEED_16MHz
 
+    FRCTL0 = FRCTLPW | NWAITS_1; // above 16Mhz needs FRAM wait states
+    CS_setDCOFreq(CS_DCORSEL_1, CS_DCOFSEL_4);
+    CS_initClockSignal (CS_MCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1);
+    CS_initClockSignal (CS_SMCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1);
 
+#endif
+
+    __bis_SR_register(GIE);
+
+    //Verify if the Clock settings are as expected
+    volatile uint32_t clockValue;
+    clockValue = CS_getMCLK();
+    clockValue = CS_getACLK();
+    clockValue = CS_getSMCLK();
+    if(clockValue);
+
+    _delay_cycles(1000);
+
+}
+
+void senseTemp(){
     while(REFCTL0 & REFGENBUSY);            //wait until busy flag is cleared
     REFCTL0 |= REFVSEL_2 | REFON;           //enable internal voltage reference for temp sensor
 
@@ -121,23 +184,48 @@ void main(void) {
     ADC12CTL3 = ADC12TCMAP;
     ADC12MCTL0 = ADC12VRSEL_1 | ADC12INCH_30;
 
-    while(1){
-        ADC12CTL0 |= ADC12ENC | ADC12SC;
-        while((ADC12IFGR0 & BIT0) == 0);
+    ADC12CTL0 |= ADC12ENC | ADC12SC;
+    while((ADC12IFGR0 & BIT0) == 0);
 
-        adc_raw = ADC12MEM0;
-        prevCel = tempCel;
+    adc_raw = ADC12MEM0;
+    prevCel = tempCel;
 
-        tempCel = (55 * ((float)adc_raw - ref30))/(ref85 - ref30) + 30;
-        UART_TX_INT(tempCel);
+    tempCel = (55 * ((float)adc_raw - ref30))/(ref85 - ref30) + 30;
+    //UART_TX_INT(tempCel);
 
-        //matMul();
-
-        //P3OUT ^= 0x01;
-
-
-    }
 }
+
+void main(void) {
+    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
+                                            // to activate previously configured port settings
+    P3DIR |= 0x01;                          // Set P3.0 to output direction
+    P3OUT = 0;
+    //P1DIR |= 0x01;                          // Set P1.0 to output direction
+
+    TA0CCTL0 = CCIE;                        // TACCR0 interrupt enabled
+    TA0CCR0 = 50000;
+    TA0CTL = TASSEL__SMCLK | MC__CONTINOUS; // SMCLK, continuous mode
+
+    __bis_SR_register(LPM0_bits | GIE);     // Enter LPM0 w/ interrupt
+
+    //clock_1();
+    clock_setup();
+    /*
+    UCA0CTLW0 = UCSWRST;                    //reset states
+    UCA0CTLW0 |= UCSSEL_2;                  //eUSCI clock source: SMCLK
+    UCA0BRW = 6;
+    UCA0MCTLW = UCOS16 | (8 << 4) | (32 << 8); //set baud rate to 9600 8N1
+    P2SEL1 |= BIT0 | BIT1;
+    P2SEL0 &= ~(BIT0 | BIT1);
+    UCA0CTLW0 &= ~UCSWRST;*/
+    __delay_cycles(2000);
+
+}
+
+int timerCounter = 0;
+int txTimer = 0;
+int txOn = 0;
 
 // Timer0_A0 interrupt service routine
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
@@ -149,10 +237,21 @@ void __attribute__ ((interrupt(TIMER0_A0_VECTOR))) Timer0_A0_ISR (void)
 #error Compiler not supported!
 #endif
 {
-    if(timerCounter == 20) {
+    if(timerCounter == 100) {
+        //senseTemp();
         P3OUT ^= BIT0;
         timerCounter = 0;
+        txTimer = 0;
+        txOn = 1;
     }
+    //tune transmission time
+    if(txOn && txTimer == 20){
+        P3OUT ^= BIT0;
+        txOn = 0;
+        txTimer = 0;
+    }
+
     TA0CCR0 += 50000;                       // Add Offset to TA0CCR0
     timerCounter ++;
+    txTimer ++;
 }
