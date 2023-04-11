333a43ad34e30b80e1e83aa2b0a5f3898cafb814
kg9903 <36208392+kg9903@users.noreply.github.com>
2023-04-11 02:49:30 -0400

    update methodology

---
+++ temp/sprinter.c
@@ -0,0 +1,11 @@
+//***************************************************************************************
+//  ACLK = n/a, MCLK = SMCLK = default DCO
+//
+//***************************************************************************************
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "driverlib/driverlib.h"
+#include <msp430.h>
+
---
+++ temp/sprinter.h
@@ -0,0 +1,18 @@
+//***************************************************************************************
+//  ACLK = n/a, MCLK = SMCLK = default DCO
+//
+//***************************************************************************************
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "driverlib/driverlib.h"
+#include <msp430.h>
+
+typedef struct Sprinter
+{
+    /* data */
+} Sprinter;
+
+
+void sInit
